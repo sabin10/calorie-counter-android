7948bcfe7c6b1fbee4ed7cce6e34360bfa48ea00
sabin10 <hantusabin10@gmail.com>
2019-06-05 13:05:05 +0300

    UI base for login&register fragments

--- app/src/main/java/com/sabinhantu/caloriecounter/Auth/LoginFragment.kt
+++ app/src/main/java/com/sabinhantu/caloriecounter/Auth/LoginFragment.kt
@@ -6,27 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.sabinhantu.caloriecounter.R
 
 class LoginFragment : Fragment() {
 
-    private lateinit var registerButton: Button
+    private lateinit var emailView: EditText
+    private lateinit var passwordView: EditText
+    private lateinit var loginButton: Button
     private lateinit var toRegisterText: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
 
-        registerButton = view.findViewById(R.id.to_register_btn)
-        registerButton.setOnClickListener {
-            Toast.makeText(activity, "button clicked", Toast.LENGTH_SHORT).show()
-        }
-
+        emailView = view.findViewById(R.id.login_email_view)
+        passwordView = view.findViewById(R.id.login_password_view)
+        loginButton = view.findViewById(R.id.login_button_view)
         toRegisterText = view.findViewById(R.id.to_register_text)
+
         toRegisterText.setOnClickListener (
             Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_registerFragment)
         )
--- app/src/main/java/com/sabinhantu/caloriecounter/Auth/RegisterFragment.kt
+++ app/src/main/java/com/sabinhantu/caloriecounter/Auth/RegisterFragment.kt
@@ -5,14 +5,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.sabinhantu.caloriecounter.R
 
 class RegisterFragment : Fragment() {
 
+    private lateinit var emailView: EditText
+    private lateinit var passwordView: EditText
+    private lateinit var passwordConfView: EditText
+    private lateinit var registerButton: Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        emailView = view.findViewById(R.id.register_email_view)
+        passwordView = view.findViewById(R.id.register_password_view)
+        passwordConfView = view.findViewById(R.id.register_passwordconf_view)
+        registerButton = view.findViewById(R.id.register_passwordconf_view)
+
+        return view
     }
 
 
