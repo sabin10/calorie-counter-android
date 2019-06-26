842c43db52f29c03c20ea1c1d80d264a2f99a8b9
sabin10 <hantusabin10@gmail.com>
2019-06-27 02:02:44 +0300

    response model succes from api

--- app/src/main/java/com/sabinhantu/caloriecounter/network/model/ResponseJson.kt
+++ app/src/main/java/com/sabinhantu/caloriecounter/network/model/ResponseJson.kt
@@ -3,9 +3,8 @@ package com.sabinhantu.caloriecounter.network.model
 import com.squareup.moshi.Json
 
 data class ResponseJson (
-    val text: String,
+    val text: String = "",
     val parsed: List<Parsed>,
     val hints: List<Hint>,
     @Json(name = "_links") val links: Links
-
 )
