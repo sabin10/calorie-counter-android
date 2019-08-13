0b935bf6e4c39ffd5d9a83c0044a04e873d6280f
Sabin Hantu <sabinhantu@gmail.com>
2019-08-13 19:50:56 +0300

    created binding adapter

--- app/src/main/java/com/sabinhantu/caloriecounter/addFood/AddFoodViewModel.kt
+++ app/src/main/java/com/sabinhantu/caloriecounter/addFood/AddFoodViewModel.kt
@@ -26,6 +26,10 @@ class AddFoodViewModel(
 
     val currentGramsString = MutableLiveData<String>()
 
+    /**
+     *  Chart values
+     */
+
     init {
         _selectedFood.value = food
         currentGramsString.value = "100"
